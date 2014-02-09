fbdd95b7672f5fe28f39241bba9d676b2dd9426c
brooks <brooksmason@gmail.com>
2014-02-08 21:52:42 -0800

    added heroku rails_12factor and layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def copyright_notice_year_range(start_year)
+    # In case the input was not a number (nil.to_i will return a 0)
+    start_year = start_year.to_i
+
+    # Get the current year from the system
+    current_year = Time.new.year
+
+    # When the current year is more recent than the start year, return a string 
+    # of a range (e.g., 2010 - 2012). Alternatively, as long as the start year 
+    # is reasonable, return it as a string. Otherwise, return the current year 
+    # from the system.
+    if current_year > start_year && start_year > 2000
+      "#{start_year} - #{current_year}"
+    elsif start_year > 2000
+      "#{start_year}"
+    else
+      "#{current_year}"
+    end
+  end
 end
